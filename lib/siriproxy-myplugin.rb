c6d3ffa6a9850d675d468cb3ea941c1ce4e5e19a
machimotako <masatako@mac.com>
2013-04-20 01:31:25 +0900

    My plugin.

--- lib/siriproxy-myplugin.rb
+++ lib/siriproxy-myplugin.rb
@@ -1,7 +1,30 @@
 require "siriproxy-myplugin/version"
+require 'cora'
+require 'siri_objects'
+require 'pp'
 
-module Siriproxy
-  module Myplugin
-    # Your code goes here...
+#module Siriproxy
+#  module Myplugin
+#    # Your code goes here...
+#  end
+#end
+
+#######
+# This is a "hello world" style plugin. It simply intercepts the phrase "test siri proxy" and responds
+# with a message about the proxy being up and running (along with a couple other core features). This
+# is good base code for other plugins.
+#
+# Remember to add other plugins to the "config.yml" file if you create them!
+######
+
+class SiriProxy::Plugin::Myplugin < SiriProxy::Plugin
+  def initialize(config)
+    #if you have custom configuration options, process them here!
   end
+
+  listen_for /あいうえお/i do
+    say "こんにちは。Siri Proxyです。"
+    request_completed #always complete your request! Otherwise the phone will "spin" at the user!
+  end
+
 end
