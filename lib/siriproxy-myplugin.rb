992b61e32999ca63b5afce1068a59e384e3f180f
machimotako <masatako@mac.com>
2013-04-28 01:41:30 +0900

    Add iremocon.

--- lib/siriproxy-myplugin.rb
+++ lib/siriproxy-myplugin.rb
@@ -2,7 +2,7 @@ require "siriproxy-myplugin/version"
 require 'cora'
 require 'siri_objects'
 require 'pp'
-require 'logger'
+require 'iremocon'
 
 #module Siriproxy
 #  module Myplugin
@@ -18,34 +18,46 @@ require 'logger'
 # Remember to add other plugins to the "config.yml" file if you create them!
 ######
 
+# 以下のプラグインを参考に、iremoconと連携しました
+# git://github.com/katsuma/SiriProxy-iRemocon.git
+
 class SiriProxy::Plugin::Myplugin < SiriProxy::Plugin
-  @@log = Logger.new("/var/log/siriproxy.log")
-  @@log.level = Logger::INFO
+  attr_accessor :host, :signals
 
   def initialize(config)
     #if you have custom configuration options, process them here!
+    self.host = config{"host"}
+    self.signals = config{"signals"}
+  end
+
+  def signal_to_iremocon(signal)
+    iremocon = ::Iremocon.new(self.host)
+    iremocon.is(signal)
+    iremocon.telnet.close
+  end
+
+  listen_for /テレビを(つけて|付けて|けして|消して)/
+    say "わかりました"
+    signal_to_iremocon(self.signals['stb']['power'])
+    request_completed
   end
 
   listen_for /あいうえお/i do
-    @@log.info("The reply to あいうえお.")
     say "こんにちは。Siri Proxyです。"
     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
   end
 
   listen_for /テスト?プラグイン/i do
-    @@log.info("The reply to テストプラグイン.")
     say "こんにちは。Siri Proxyです。"
     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
   end
 
   listen_for /test?hi/i do
-    @@log.info("The reply to test hi.")
     say "こんにちは。Siri Proxyです。"
     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
   end
 
   listen_for /hi/i do
-    @@log.info("The reply to hi.")
     say "This is Siri Proxy."
     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
   end
